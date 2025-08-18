6b7f7d3a4f209810fb2596edb7022ece138ee96e
SwaroopaDhepe <swaroopadhepe7@gmail.com>
2025-08-18 16:17:22 +0530

    Update Load Balancing.cpp

--- Assignment3/Load Balancing.cpp
+++ Assignment3/Load Balancing.cpp
@@ -1,3 +1,10 @@
+/*Load Balancing:
+For example, imagine you have a set of servers that handle requests for a web application. The
+key to load balancing is using the hash value of a client's IP address or a request ID to
+determine which server should handle the request. The hash function is typically designed so
+that the data is evenly distributed across the servers, ensuring that no single server is
+overloaded. Write a program of a load balancing system*/
+
 #include<iostream>
 using namespace std;
 int main(){
